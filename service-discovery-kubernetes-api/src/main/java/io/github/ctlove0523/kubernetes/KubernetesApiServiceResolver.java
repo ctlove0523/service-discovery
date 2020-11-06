d7401797eeaed05657a9d47f813811625ed4b0c5
ctlove0523 <478309639@qq.com>
2020-11-07 07:38:48 +0800

    support kubernetes api

--- service-discovery-kubernetes-api/src/main/java/io/github/ctlove0523/kubernetes/KubernetesApiServiceResolver.java
+++ service-discovery-kubernetes-api/src/main/java/io/github/ctlove0523/kubernetes/KubernetesApiServiceResolver.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import io.github.ctlove0523.commons.UrlUtil;
 import io.github.ctlove0523.discovery.api.Instance;
 import io.github.ctlove0523.discovery.api.ServiceResolver;
+import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.openapi.models.V1Service;
 import okhttp3.Request;
 
@@ -14,8 +15,8 @@ public class KubernetesApiServiceResolver implements ServiceResolver {
 	private static final String DEFAULT_NAME_SPACE = "default";
 	private String apiServerHost;
 	private String token;
-	private static final String QUERY_SERVICE_URL = "GET /api/v1/namespaces/{namespace}/services/{name}";
-
+	private static final String QUERY_SERVICE_URL = "/api/v1/namespaces/{namespace}/services/{name}";
+	private static final String LIST_POD_URL = "api/v1/namespaces/{namespace}/pods";
 
 	@Override
 	public List<Instance> resolve(String name) {
@@ -40,5 +41,25 @@ public class KubernetesApiServiceResolver implements ServiceResolver {
 
 		return HttpsClient.syncCall(request, V1Service.class);
 
+	}
+
+	private V1PodList listPodsBySelector(String namespace, String selector) {
+		if (namespace == null || namespace.isEmpty()) {
+			namespace = DEFAULT_NAME_SPACE;
+		}
+
+		String url = UrlUtil.urlPathFormat(LIST_POD_URL, namespace);
+		Map<String, String> queryParas = new HashMap<>();
+		queryParas.put("pretty", "true");
+		queryParas.put("labelSelector", selector);
+		url = UrlUtil.urlQueryParametersFormat(url, queryParas);
+
+		Request request = new Request.Builder()
+				.get()
+				.url(apiServerHost + url)
+				.addHeader("Authorization", "Bearer " + token)
+				.build();
+
+		return HttpsClient.syncCall(request, V1PodList.class);
 	}
 }
