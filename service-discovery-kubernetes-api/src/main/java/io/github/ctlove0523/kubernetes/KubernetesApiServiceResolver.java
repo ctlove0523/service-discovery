ab297485c0085bbc8dd1f9ed1c86d6a29c8f85bf
ctlove0523 <478309639@qq.com>
2020-11-09 08:11:09 +0800

    add method to get instances

--- service-discovery-kubernetes-api/src/main/java/io/github/ctlove0523/kubernetes/KubernetesApiServiceResolver.java
+++ service-discovery-kubernetes-api/src/main/java/io/github/ctlove0523/kubernetes/KubernetesApiServiceResolver.java
@@ -1,19 +1,27 @@
 package io.github.ctlove0523.kubernetes;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import io.github.ctlove0523.commons.UrlUtil;
 import io.github.ctlove0523.discovery.api.Instance;
+import io.github.ctlove0523.discovery.api.InstanceAddress;
 import io.github.ctlove0523.discovery.api.ServiceResolver;
+import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.openapi.models.V1PodStatus;
 import io.kubernetes.client.openapi.models.V1Service;
+import io.kubernetes.client.openapi.models.V1ServiceSpec;
 import okhttp3.Request;
 
 public class KubernetesApiServiceResolver implements ServiceResolver {
 	private static final String DEFAULT_NAME_SPACE = "default";
 	private String apiServerHost;
+	private String namespace;
 	private String token;
 	private static final String QUERY_SERVICE_URL = "/api/v1/namespaces/{namespace}/services/{name}";
 	private static final String LIST_POD_URL = "api/v1/namespaces/{namespace}/pods";
@@ -21,6 +29,36 @@ public class KubernetesApiServiceResolver implements ServiceResolver {
 	@Override
 	public List<Instance> resolve(String name) {
 		return null;
+	}
+
+	private List<Instance> lookup(String name) {
+		V1Service service = showService(namespace, name);
+		if (service == null) {
+			return new ArrayList<>();
+		}
+
+		V1ServiceSpec v1ServiceSpec = service.getSpec();
+		if (v1ServiceSpec == null) {
+			return new ArrayList<>();
+		}
+
+		// todo map selector to string format
+		Map<String, String> selector = v1ServiceSpec.getSelector();
+		String stringFormatSelector = "";
+
+		V1PodList v1PodList = listPodsBySelector(namespace, stringFormatSelector);
+		if (v1PodList == null || v1PodList.getItems() == null || v1PodList.getItems().isEmpty()) {
+			return new ArrayList<>();
+		}
+
+		return v1PodList.getItems().stream()
+				.filter(Objects::nonNull)
+				.map(V1Pod::getStatus)
+				.filter(Objects::nonNull)
+				.map(V1PodStatus::getPodIP)
+				.filter(Objects::nonNull)
+				.map(s -> (new Instance(new InstanceAddress(s))))
+				.collect(Collectors.toList());
 	}
 
 	private V1Service showService(String namespace, String name) {
